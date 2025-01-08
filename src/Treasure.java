f7a148557de3df985c686ec1f6a207f32817ffb0
BT_2S14_31 <gabriellej34@nycstudents.net>
2025-01-08 10:27:05 -0500

    TH-1: Implemented the Treasure class and its methods into the TreasureHunter Class.

--- src/Treasure.java
+++ src/Treasure.java
@@ -5,12 +5,16 @@ public class Treasure {
     private int cTreasure;
     private int t = 0;
     private boolean treasureFound;
+    private String treasure;
+
+    private Hunter inv;
 
     public Treasure() {
         this.aTreasure = 0;
         this.bTreasure = 0;
         this.cTreasure = 0;
         this.treasureFound = false;
+        this.treasure = "Treasure Chest";
 
     }
 
@@ -21,23 +25,14 @@ public class Treasure {
     public boolean lookingForTreasure() {
 
 
-        int chance;
-        if (Math.random() < .5) {
-
-            chance = 1;
-
-        } else {
-
-            chance = 2;
-
-        }
-
-        if (chance == 1){
+        int chance = (int) (1 + Math.random() * 2);
+        if (chance == 1) {
 
             treasureFound = false;
 
         }
-        if (chance == 2){
+
+        if (chance == 2) {
 
             treasureFound = true;
             t++;
@@ -45,41 +40,53 @@ public class Treasure {
 
             if (whichOne == 1) {
                 aTreasure = 1;
+                treasureFound = false;
             }
 
             if (whichOne == 2) {
                 bTreasure = 1;
+                treasureFound = false;
             }
 
             if (whichOne == 3) {
                 cTreasure = 1;
+                treasureFound = false;
+
             }
+
+
         }
 
-        return false;
-    }
-
-    public boolean treasureFound() {
         return treasureFound;
     }
 
-    public String congrats() {
-        String str = "";
-       if  (aTreasure == 1 && treasureFound) {
-           return str = "You have found this unique treasure. It contains: ." + "\n" +
-                   " Total amount of treasure found: " + t;
+    public void addToInventory(String item)
+    {
+
+
+    }
+
+
+    public void congrats() {
+
+       if  (aTreasure == 1) {
+           System.out.println("You have found this unique treasure. It contains: ." + "\n" +
+                   " Total amount of treasure found: " + t);
+           aTreasure = 0;
        }
-       if  (bTreasure == 1 && treasureFound) {
-           return str = "You have found this unique treasure. It contains: ." + "\n" +
-                   " Total amount of treasure found: " + t;
+       if  (bTreasure == 1) {
+           System.out.println("You have found this unique treasure. It contains: ." + "\n" +
+                   " Total amount of treasure found: " + t);
+           bTreasure = 0;
        }
-       if  (cTreasure == 1 && treasureFound) {
-           return str = "You have found this unique treasure. It contains: ." + "\n" +
-                   " Total amount of treasure found: " + t;
+       if  (cTreasure == 1) {
+           System.out.println("You have found this unique treasure. It contains: ." + "\n" +
+                   " Total amount of treasure found: " + t);
+           cTreasure = 0;
        }
        if (t == 3) {
-           return str = "You have found all three unique treasures! Congratulations!";
+           System.out.println("You have found all three unique treasures! Congratulations!");
        }
-        return str;
+
     }
 }
